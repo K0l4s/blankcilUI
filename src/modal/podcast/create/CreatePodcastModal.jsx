527525b49c97958b69fb5c8d17c9e84e4b628c9c
K0l4s <21110223@student.hcmute.edu.vn>
2024-05-06 01:29:12 +0700

    create add podcast layout

---
+++ src/modal/podcast/create/CreatePodcastModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CreatePodcastModal.css'
+const CreatePodcastModel = () => {
+  return (
+    <div>CreatePodcastModel</div>
+  )
+}
+
+export default CreatePodcastModel
