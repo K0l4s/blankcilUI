dd28a8bea7400af17a850c6dd5bbe4a9ef1a83f6
K0l4s <21110223@student.hcmute.edu.vn>
2024-05-06 01:28:02 +0700

    config meta header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 import { AudioPlayerProvider } from './components/post/podcastPost/PodcastContext';
 import Router from './page/router/Router';
-
+import favicon from './favicon.ico';
 function App() {
   const mode = localStorage.getItem('themeMode') || 'dark';
   if(mode === 'dark') {
@@ -13,8 +13,22 @@ function App() {
     document.body.style.backgroundColor = 'white';
     document.body.style.color = 'black';
   }
+  document.title = 'Blankcil';
+  document.querySelector('link[rel="icon"]').href = favicon;
+  document.querySelector('link[rel="apple-touch-icon"]').href = favicon;
+  document.querySelector('meta[name="description"]').content = "Blankcil là một trang web cho phép người dùng tạo ra các podcast chữa lành và đẩy healing.";
+
+  // Kiểm tra nếu không có video nào đang chạy thì phát âm thanh nền
+  // useEffect(() => {
+  //   if (audioRefs.current.every((audio) => !audio.current?.paused)) {
+  //     audioRefs.current[0].current.play();
+  //   }
+  // }, [audioRefs]);
+  
   return (
     <div className="App">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <meta name="description" content="Blankcil là một trang web cho phép người dùng tạo ra các podcast chữa lành và đẩy healing."></meta>
       <ChakraProvider>
       <AudioPlayerProvider>
       <Router />
