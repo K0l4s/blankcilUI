7f3d33230cd710e700068972d5f3bfcf43964d7a
K0l4s <21110223@student.hcmute.edu.vn>
2024-05-07 09:19:03 +0700

    edit leftbar

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Leftbar.css'
 import { AiOutlineSearch } from "react-icons/ai";
 import { IoMdNotifications } from "react-icons/io";
 import { FaRegBookmark } from "react-icons/fa";
 import { LuMessagesSquare } from "react-icons/lu";
 import { useNavigate } from 'react-router-dom';
+import { RiVideoAddLine } from 'react-icons/ri';
+import CreatePodcastModal from '../../modal/podcast/create/CreatePodcastModal';
 
 const Leftbar = () => {
   const navigate = useNavigate();
+  const [isOpenAdd, setIsOpenAdd] = useState(false);
+  const handleOpenAdd = () => {
+    if(!isOpenAdd)
+      document.title="Create Podcast - Blankcil";
+    else
+      document.title="Blankcil";
+    setIsOpenAdd(!isOpenAdd);
+    console.log("open");
+  }
   return (
     <div className='leftbar'>
         <div onClick={()=>navigate("/blankcilUI/profile")} className='item'><img src="https://t4.ftcdn.net/jpg/02/90/67/89/360_F_290678971_Bk11xnoP5lQw4US7wCSId6jcKmWSfDBg.jpg" alt="" /><p>Profile</p></div>
@@ -15,8 +26,10 @@ const Leftbar = () => {
         <div className='item'><AiOutlineSearch/><p>Tìm kiếm</p></div>
         <div className='item'><LuMessagesSquare/><p>Nhắn tin</p></div>
         <div className='item'><IoMdNotifications/><p>Thông báo</p></div>
+        <div className='item' onClick={handleOpenAdd}><RiVideoAddLine /><p>Thêm</p></div>
         <div className='item'><FaRegBookmark/><p>Đã lưu</p></div>
         <div>MORE</div>
+        <CreatePodcastModal isOpen={isOpenAdd} onClose={handleOpenAdd}/>
     </div>
   )
 }
