f8361509498926851fe171580380bc0ee5166b41
K0l4s <21110223@student.hcmute.edu.vn>
2024-05-11 05:48:28 +0700

    config and add api for search user

--- src/components/search/user/SearchUser.jsx
+++ src/components/search/user/SearchUser.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
-const SearchUser = () => {
+import './SearchUser.css'
+import { GrView } from 'react-icons/gr'
+import { FcLike } from 'react-icons/fc'
+const SearchUser = (user) => {
   return (
-    <div>SearchUser</div>
+    <div className='userBox'>
+      <div className='userInfor'>
+        <img src="https://cdn.tgdd.vn/hoi-dap/1314184/podcast-la-gi-co-gi-thu-vi-nghe-podcast-o-dau-2-1.jpg" alt="" className="img" />
+        <div className="text">
+          <h3>{user.user.fullname}</h3>
+          <p>Có {user.user.podcasts.length} podcast</p>
+        </div>
+      </div>
+      <div className="action">
+        <button><GrView/></button>
+        <button><FcLike/></button>
+      </div>
+    </div>
   )
 }
 
