61837857ba82bed2d4df53d79e9282ea040912b4
Trung Kien <21110223@student.hcmute.edu.vn>
2024-06-05 05:06:39 +0700

    add setting page

---
+++ src/page/setting/SettingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './SettingPage.css'
+const SettingPage = () => {
+  return (
+    <div>
+        <div className='setting'>
+            <h1>Setting</h1>
+            <div className="settingItem">
+                <p>Change password</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SettingPage
