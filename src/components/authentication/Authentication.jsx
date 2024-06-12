35b12640f57b6f889a2cdb8c56437bd68816d73e
Trung Kien <21110223@student.hcmute.edu.vn>
2024-06-13 01:35:08 +0700

    update api

--- src/components/authentication/Authentication.jsx
+++ src/components/authentication/Authentication.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './Authentication.css'
-import Login from './login/Login'
+import React, { useEffect, useState } from 'react';
+import './Authentication.css';
+import Login from './login/Login';
 import Register from './register/Register';
+import { isTokenExpired } from '../../useAuth';
+
 const Authentication = ({ isOpen, onClose, login }) => {
     const [isLogin, setIsLogin] = useState(login);
-    useEffect(() => {
-        if (isOpen)
-            document.body.style.overflow = 'hidden';
-        else
-            document.body.style.overflow = 'auto';
-    }, [isOpen])
-
-    if (!isOpen || isOpen === false) return null
-
+    
+    if(!isOpen) 
+        return;
+const token = localStorage.getItem('access_token');
+    if (token && !isTokenExpired(token)) {
+        onClose();
+        return;
+    }
     const handleIsLogin = () => {
         setIsLogin(!isLogin);
-    }
+    };
     return (
         <>
             <div className="overlay" onClick={onClose}></div>
             <div className='authen'>
                 <div className="closeBtn" onClick={onClose}>X</div>
                 {isLogin ? (
-                    <Login handleIsLogin={handleIsLogin}/>
-                )
-                    : (
-                        <Register handleIsLogin={handleIsLogin}/>
-                    )}
+                    <Login handleIsLogin={handleIsLogin} onClose={onClose} />
+                ) : (
+                    <Register handleIsLogin={handleIsLogin} />
+                )}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Authentication
+export default Authentication;
